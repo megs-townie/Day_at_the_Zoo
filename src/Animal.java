2e5dbc00aa78b1cc42bfb7cb97e54c824dec57a9
Meagan Townsend <meagan.townsend337@gmail.com>
2023-02-03 08:05:33 -0400

    Exercise 3 ready for marking

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,77 @@
-package PACKAGE_NAME;public class Animal {
+class Animal {
+
+    //region Variables
+    private String species;
+    private int maxWeight;
+    private String habitat;
+    private boolean isEndangered;
+    //endregion
+
+    //region Constructor
+    public Animal() {
+        this.species = "Unknown";
+        this.maxWeight = 0;
+        this.habitat = "Unknown";
+        this.isEndangered = false;
+    }
+
+    public Animal(String species, int maxWeight, String habitat, boolean isEndangered) {
+        this.species = species;
+        this.maxWeight = maxWeight;
+        this.habitat = habitat;
+        this.isEndangered = isEndangered;
+    }
+
+    // endregion
+
+    //region Getters and Setters
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public int getMaxWeight() {
+        return maxWeight;
+    }
+
+    public void setMaxWeight(int maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public boolean isEndangered() {
+        return isEndangered;
+    }
+
+    public void setEndangered(boolean endangered) {
+        isEndangered = endangered;
+    }
+
+    //endregion
+
+    //region Animal Status
+
+    public void feed() {
+        System.out.println(species + " has been fed");
+    }
+
+    public void makeSound() {
+        System.out.println(species + " makes a sound");
+    }
+
+    public String toString() {
+        return "I am a " + maxWeight + "lb " + species + " that lives in the " + habitat + ". I am an endangered species: " + isEndangered;
+    }
+    //endregion
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
+import java.util.Arrays;
+
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Animal lion = new Animal("Lion", 500, "Jungle", true);
+        Animal cow = new Animal("Cow", 1000, "Field", false);
+        Animal sloth = new Animal("Sloth", 100, "Trees", true);
+
+        Animal[] zoo = {lion, cow, sloth};
+
+        for (Animal animal : zoo) {
+            animal.feed();
+            animal.makeSound();
+            System.out.println(animal.toString());
+            System.out.println();
+        }
+
+        for (int i = 0; i < zoo.length; i++) {
+            Animal animal = zoo[i];
+            System.out.println("Putting " + animal.getSpecies() + " back into the zoo.");
+        }
     }
 }
+
